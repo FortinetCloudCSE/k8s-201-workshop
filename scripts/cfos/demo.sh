5850e48c1bea5b9634ab41abe28b8c55ea835438
Andy Wang <yagosys@gmail.com>
2024-04-24 19:45:46 +0800

    add east-west

--- scripts/cfos/demo.sh
+++ scripts/cfos/demo.sh
@@ -1,1 +1,9 @@
+#ingress protection
 curl http://k8strainingmaster1.westus.cloudapp.azure.com:8888/
+
+#east-west protection
+
+kubectl run curl-pod --image=curlimages/curl --restart=Never -- curl cfos7210250-service.default.svc.cluster.local:8888/ 
+kubectl logs curl-pod
+kubectl delete pod curl-pod
+
