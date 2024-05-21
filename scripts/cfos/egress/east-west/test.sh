25cb1c1533dcc916762575acd1b9a8487feebe4c
Andy Wang <yagosys@gmail.com>
2024-05-21 13:48:18 +0800

    init

--- scripts/cfos/egress/east-west/test.sh
+++ scripts/cfos/egress/east-west/test.sh
@@ -1,4 +1,4 @@
-[ok]k exec -it po/diag100 -- curl   http://10.1.200.21
-[blocked] k exec -it po/diag100 -- curl  -H "User-Agent: () { :; }; /bin/ls" http://10.1.200.21
+[ok]k exec -it po/diag100 -n app-2 -- curl   http://10.1.200.21
+[blocked] k exec -it po/diag100 -n app-2  -- curl  -H "User-Agent: () { :; }; /bin/ls" http://10.1.200.21
 
 
