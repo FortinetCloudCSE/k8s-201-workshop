e6e1e960bf1a9440b6c53eaf78a8f8effef5e415
Andy Wang <yagosys@gmail.com>
2024-07-25 14:44:11 +0800

    add ingress yaml

--- scripts/openshift-aro/fortiweb/ingressdemo.sh
+++ scripts/openshift-aro/fortiweb/ingressdemo.sh
@@ -1,6 +1,7 @@
 ./deploy_fortiweb_expose_slb.sh 
 ./deploy_fortiwebingresscontroller.sh 
 ./deploy_demo_application.sh 
+kubectl apply -f ingress.yaml
 ip=$(kubectl get cm ssh-config -o json | jq -r .data.SSH_HOST)
 echo $ip
 port="8888"
