dbc8df0662d6443cca39c94f6f3f497784c04447
Andy Wang <yagosys@gmail.com>
2024-07-26 12:31:25 +0800

    modify fqdn variable assignment

--- scripts/openshift-aro/ingressdemo/ingressdemo.sh
+++ scripts/openshift-aro/ingressdemo/ingressdemo.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -x
 cfosnamespace="cfos-ingress"
 
+echo delete namespace cfos-ingress
+kubectl delete namespace $cfosnamespace
+
+echo delete target application and svc
+kubectl delete svc goweb
+kubectl delete svc nginx
+kubectl delete deployment goweb
+kubectl delete deployment nginx
+
+echo create again
+
 
 ./create_cfos.sh
 ./create_app.sh
@@ -8,12 +19,15 @@ cfosnamespace="cfos-ingress"
 
 echo sleep 60
 sleep 60
-echo get rg name
-arogroupname=$(az group list -o table | grep aro | cut -d ' ' -f 1)
+
+# Get resource group name
+arogroupname=$(az group list --query "[?contains(name, 'aro')].name" -o tsv)
 echo $arogroupname
-echo get fqdn name
-fqdn=$(az network public-ip list -g $arogroupname | grep fqdn | cut -d ":" -f 2 | tr -d '"')
+
+# Get FQDN name
+fqdn=$(az network public-ip list -g "$arogroupname" --query "[].dnsSettings.fqdn" -o tsv)
 echo $fqdn
+
 
 curl -v http://${fqdn}:8080
 curl -v http://${fqdn}:8000
@@ -22,6 +36,7 @@ curl -v http://${fqdn}:8005
 wget -c https://secure.eicar.org/eicar_com.zip
 
 cfosnamespace="cfos-ingress"
+scriptDir="${HOME}/github"
 curl -v --max-time 5 -F "file=@$scriptDir/k8s-201-workshop/scripts/cfos/ingress_demo/eicar_com.zip" http://${fqdn}:8000
 podname=$(kubectl get pod -n $cfosnamespace -l app=cfos -o jsonpath='{.items[*].metadata.name}')
 echo $podname
