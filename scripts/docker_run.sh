c7822d70231788a7f6582a7d57080508eb49a670
srijaallam <srija.allam10@gmail.com>
2024-08-19 12:54:46 -0400

    docker updates

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -5,9 +5,9 @@ myarray=( "build" "server" "shell" "" )
 [[ "$#" > "1" ]] || [[ ! " ${myarray[*]} " =~ " $1 " ]] && echo "Usage: ./scripts/docker_run.sh [ build | server | shell ]" && exit 1
 
 cmd="docker run --rm -it 
-  -v $(pwd)/content:/home/CentralRepo/content 
-  -v $(pwd)/docs:/home/CentralRepo/public 
-  --mount type=bind,source=$(pwd)/config.toml,target=/home/CentralRepo/config.toml 
+  -v /Users/srijareddyallam/Documents/TECworkshop/k8s-201-workshop/content:/home/CentralRepo/content
+  -v /Users/srijareddyallam/Documents/TECworkshop/k8s-201-workshop/docs:/home/CentralRepo/public
+  --mount type=bind,source=/Users/srijareddyallam/Documents/TECworkshop/k8s-201-workshop/config.toml,target=/home/CentralRepo/config.toml
   -p 1313:1313 fortinet-hugo:latest $1"
 echo $cmd
 $cmd
